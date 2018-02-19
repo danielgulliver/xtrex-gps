0c5dbc530f90ad7fdd2e06d8417fd566dda5a6ed
Connor Harris <harrishedge@gmail.com>
2018-02-19 20:25:37 +0000

    GPS classes and interface + a file I/O for logging ect.

---
+++ xtrex/src/main/java/aprogrammerisneverlate/xtrex/FileWritter.java
@@ -0,0 +1,91 @@
+package aprogrammerisneverlate.xtrex;
+
+import java.io.*;
+import java.nio.file.Paths;
+
+public class FileWritter {
+    private static BufferedWriter bw = null;
+    private static FileWriter fw = null;
+    private static String LogPath;
+    
+    public FileWritter () {
+        LogPath = Paths.get(".").toAbsolutePath().normalize().toString();
+        Logging(true); 
+    }
+    
+        static void Logging (boolean start) {
+        LogPath = Paths.get(".").toAbsolutePath().normalize().toString();
+        if (start) {
+            try {
+                fw = new FileWriter(LogPath + "\\log.txt");
+                bw = new BufferedWriter(fw);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                bw.flush();
+                bw.close();
+                fw.close();
+            } catch (IOException exc) {
+                exc.printStackTrace();
+            }
+        }
+    }
+    
+    public static void Logger (String log) {
+        try {
+            bw.write(log);
+            bw.newLine();
+            bw.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public static void Logger (byte[] dirs) {
+        for ( int i = 0; i < dirs.length; i++ ) {
+            try {
+                bw.write(dirs[i]);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
+/**
+public void writeGridToFile() throws IOException {
+            FileWriter fw = new FileWriter("D:/GridArrayFile.txt");
+            try {
+                for(int i = 0; i < 100; i++){
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    bw.write(GridArray[i]);
+                    bw.close();
+                }
+            }
+
+            catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Error");
+            }
+            finally {
+                //TODO
+                System.out.println("Finished");
+            }
+    }
+public void writeGridToFile() throws IOException {
+    try(BufferedWriter bw = new BufferedWriter(new FileWriter("D:/GridArrayFile.txt"))) {
+        for(int i = 0; i < 100; i++){
+            bw.write(Integer.toString(GridArray[i]));
+       }
+        bw.flush();
+     }
+     catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error");
+     }
+     finally {
+            System.out.println("Finished");
+     }
+}
+*/
---
+++ xtrex/src/main/java/aprogrammerisneverlate/xtrex/GPSinterface.java
@@ -0,0 +1,13 @@
+package aprogrammerisneverlate.xtrex;
+
+public interface GPSinterface {
+
+    public boolean GPS();
+
+    public float Latitude();
+
+    public float Longitude();
+    
+    public float GPStime();
+
+}
