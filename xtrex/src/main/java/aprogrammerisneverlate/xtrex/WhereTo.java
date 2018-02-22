8ad997b8edd6864b74dfa177ef8a22483f5ce825
Daniel Gulliver <djag201@exeter.ac.uk>
2018-02-22 01:08:21 +0000

    Refactor `WhereTo.java` to make use of `SelectionController`

--- xtrex/src/main/java/aprogrammerisneverlate/xtrex/WhereTo.java
+++ xtrex/src/main/java/aprogrammerisneverlate/xtrex/WhereTo.java
@@ -1,126 +1,149 @@
 package aprogrammerisneverlate.xtrex;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.text.BadLocationException;
 
-
+/**
+ * The 'Where To?' screen enables the user to type in their destination, identified either by name or by postcode.
+ * The screen has a destination field and two keyboards which are used to enter text into the field.
+ * <p>
+ * True to the behaviour of the original device, the user may cycle through the keyboard's buttons using the plus
+ * and minus buttons, and select a button using the Select button. However, in our simulation of the device, it is
+ * also possible to click directly on the buttons on the keyboard to enter the destination, or even type the destination
+ * directly into the destination field.
+ * 
+ * @author Daniel Gulliver
+ */
 public class WhereTo extends Screen {
 	
-	private Keyboard keyboard1 = new Keyboard1(), keyboard2 = new Keyboard2(), currentKeyboard;
-	private int buttonNumber = 0;
+	private static final long serialVersionUID = 2063996011118360800L;
+	private Keyboard currentKeyboard, keyboard1, keyboard2;
 	private JTextPane destinationField;
 	
 	protected WhereTo() {
 		super();
 		setLayout(new BorderLayout());
-		
+
+		// Create destination field.
 		destinationField = new JTextPane();
-		add(destinationField, BorderLayout.NORTH);
+		destinationField.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
+		add(destinationField, BorderLayout.PAGE_START);
+
+
+		// Create first keyboard:
+		ArrayList<PrefabButton> keyboard1Buttons = new ArrayList<PrefabButton>();
 		
-		setKeyboard(keyboard1);
-	}
-	
-	public void addCharToDisplay(char ch) {
-		destinationField.setText(destinationField.getText() + ch);
-	}
-	
-	public void deleteCharFromDisplay() {
-		try {
-			destinationField.setText(destinationField.getText(0, destinationField.getText().length() - 1));
-		} catch (BadLocationException e) {
-			e.printStackTrace();
+		// Create character buttons for keyboard 1.
+		char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_".toCharArray();
+		for (char letter : letters) {
+			keyboard1Buttons.add(new CharacterButton(letter));
 		}
+
+		// Create button to switch to second keyboard for keyboard 1.
+		//keyboard1Buttons.add(new SwitchKeyboardButton("=>", keyboard2));
+		
+		keyboard1 = new Keyboard(keyboard1Buttons, 7, 4);
+
+
+		// Create second keyboard:
+		ArrayList<PrefabButton> keyboard2Buttons = new ArrayList<PrefabButton>();
+		
+		// Create character buttons.
+		char[] numbers = "1234567890".toCharArray();
+		for (char number : numbers) {
+			keyboard2Buttons.add(new CharacterButton(number));
+		}
+
+		// Create button to switch to first keyboard.
+		//keyboard2Buttons.add(new SwitchKeyboardButton("<=", WhereTo.this.keyboard1));
+
+		// Create delete button.
+		keyboard2Buttons.add(new DeleteButton());
+
+		keyboard2 = new Keyboard(keyboard2Buttons, 5, 3);
+
+		setKeyboard(keyboard1);
+			
 	}
 	
+	/**
+	 * Changes the keyboard on the screen. Removes the old keyboard from the screen and shows the new keyboard on the screen.
+	 * 
+	 * @param Keyboard kbd -- The keyboard to display on the screen
+	 */
 	public void setKeyboard(Keyboard kbd) {
 		if (currentKeyboard != null) {
 			remove(currentKeyboard);
-			currentKeyboard.buttons[buttonNumber].selected(false);
 		}
-		buttonNumber = 0;
 		currentKeyboard = kbd;
-		add(currentKeyboard, BorderLayout.SOUTH);
-		currentKeyboard.buttons[0].selected(true);
+		add(currentKeyboard, BorderLayout.CENTER);
 		revalidate();
 		repaint();
 	}
 
-	public void onPowerButtonPressed() {
-		System.out.println("Power button clicked");
-	}
-
-	public void onMenuButtonPressed() {
-		System.out.println("Menu button clicked");
-	}
-
 	public void onMinusButtonPressed() {
-		if (buttonNumber > 0) {
-			currentKeyboard.buttons[buttonNumber].selected(false); // deselect previously highlighted button
-			buttonNumber--;
-		}		
-		currentKeyboard.buttons[buttonNumber].selected(true);
+		// Highlight the previous button on the keyboard on the screen.
+		currentKeyboard.sc.back();
 	}
 
 	public void onPlusButtonPressed() {
-		if (buttonNumber < currentKeyboard.buttons.length - 1) {
-			currentKeyboard.buttons[buttonNumber].selected(false); // deselect previously highlighted button
-			buttonNumber++;
-		}
-		
-		currentKeyboard.buttons[buttonNumber].selected(true);
+		// Highlight the next button on the keyboard on the screen.
+		currentKeyboard.sc.forward();
 	}
 
 	public void onSelectButtonPressed() {
-		if (buttonNumber >= 0 && buttonNumber < currentKeyboard.buttons.length - 1) {
-			String btnText = currentKeyboard.buttons[buttonNumber].getText();
-			if (btnText == "=>") {
-				setKeyboard(keyboard2);
-			} else if (btnText == "<=") {
-				setKeyboard(keyboard1);
-			} else if (btnText == "DEL") {
-				deleteCharFromDisplay();
-			} else {
-				char charToAdd = btnText.charAt(0);
-				if (charToAdd == '_') charToAdd = ' ';
-				addCharToDisplay(charToAdd);
-			}
-		}
+		// Click the highlighted button to perform its associated action.
+		currentKeyboard.sc.click();
 	}
 
 	class CharacterButton extends PrefabButton {
+		private static final long serialVersionUID = 6752238440776770911L;
 		private char keyValue;
 		
 		CharacterButton(char keyValue) {
 			super("" + keyValue);
-			if (keyValue == ' ') setText("_");
-			this.keyValue = keyValue;
+			if (keyValue == '_')
+				this.keyValue = ' ';
+			else
+				this.keyValue = keyValue;
 		}
 
 		@Override
 		public void action() {
-			WhereTo.this.addCharToDisplay(this.keyValue);
+			// Add the relevant character to the display.
+			WhereTo.this.destinationField.setText(destinationField.getText() + keyValue);
 		}
 	}
 
 	class DeleteButton extends PrefabButton {
+		private static final long serialVersionUID = -2056965874020110981L;
+
 		DeleteButton() {
 			super("DEL");
 		}
 
 		@Override
 		public void action() {
-			WhereTo.this.deleteCharFromDisplay();
+			// Remove the last character from the display.
+			try {
+				String newDestinationText = destinationField.getText(0, destinationField.getText().length() - 1);
+				WhereTo.this.destinationField.setText(newDestinationText);
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
 	class SwitchKeyboardButton extends PrefabButton {
+		private static final long serialVersionUID = -7789589360533656032L;
 		Keyboard keyboardToSwitchTo;
 
 		SwitchKeyboardButton (String displayString, Keyboard keyboardToSwitchTo) {
@@ -134,92 +157,29 @@ public class WhereTo extends Screen {
 		}
 	}
 
-	class Keyboard extends JPanel {
-		public PrefabButton[] buttons;
-	}
+	/**
+	 * A keyboard is a collection of buttons which enables the user to enter text into the destination field.
+	 * Buttons are added to each keyboard during construction and cannot be added on the fly.
+	 */
+	class Keyboard extends JPanel implements ActionListener {
+		private static final long serialVersionUID = 6403469078457676699L;
+		public SelectionController sc;
+		public ArrayList<PrefabButton> buttons;
 
-	class Keyboard1 extends Keyboard {
-		
-		Keyboard1() {
-			setLayout(new GridLayout(7, 4));
-			this.buttons = new PrefabButton[28];
-			
-			char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-			int i = 0;
-			for (final char letter : letters) {
-				buttons[i] = new PrefabButton("" + letter);
-				buttons[i].addActionListener(new ActionListener() {
+		Keyboard(ArrayList<PrefabButton> buttons, int rows, int cols) {
+			setLayout(new GridLayout(rows, cols));
+			buttons.addAll(buttons);
+			sc = new SelectionController(buttons);
 
-					public void actionPerformed(ActionEvent e) {
-						addCharToDisplay(letter);
-					}
-					
-				});
-				this.add(buttons[i]);
-				i++;
+			// Add the buttons to the keyboard.
+			for (PrefabButton button : buttons) {
+				add(button);
+				button.addActionListener(this);
 			}
-			buttons[26] = new PrefabButton("_");
-			buttons[26].addActionListener(new ActionListener() {
+		}
 
-				public void actionPerformed(ActionEvent e) {
-					addCharToDisplay(' ');
-				}
-				
-			});
-			this.add(buttons[26]);
-			
-			buttons[27] = new PrefabButton("=>");
-			buttons[27].addActionListener(new ActionListener() {
-
-				public void actionPerformed(ActionEvent e) {
-					setKeyboard(keyboard2);
-				}
-				
-			});
-			this.add(buttons[27]);
+		public void actionPerformed(ActionEvent e) {
+			((PrefabButton) e.getSource()).action();
 		}
 	}
-
-	class Keyboard2 extends Keyboard {
-		Keyboard2() {
-			this.buttons = new PrefabButton[12];
-			
-			setLayout(new GridLayout(5, 3));
-			
-			char[] numbers = "1234567890".toCharArray();
-			int i = 0;
-			for (final char number : numbers) {
-				buttons[i] = new PrefabButton("" + number);
-				buttons[i].addActionListener(new ActionListener() {
-
-					public void actionPerformed(ActionEvent e) {
-						addCharToDisplay(number);
-					}
-					
-				});
-				this.add(buttons[i]);
-				i++;
-			}
-			buttons[10] = new PrefabButton("<=");
-			buttons[10].addActionListener(new ActionListener() {
-
-				public void actionPerformed(ActionEvent e) {
-					setKeyboard(keyboard1);
-				}
-				
-			});
-			this.add(buttons[10]);
-			
-			buttons[11] = new PrefabButton("DEL");
-			buttons[11].addActionListener(new ActionListener() {
-
-				public void actionPerformed(ActionEvent e) {
-					deleteCharFromDisplay();
-				}
-				
-			});
-			this.add(buttons[11]);
-		}
-	}
-	
 }
