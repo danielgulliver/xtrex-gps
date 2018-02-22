a27eef4311a39f15ed3a38a592035120f6683472
Connor Harris <harrishedge@gmail.com>
2018-02-22 20:04:42 +0000

    Correctly implemented Win7Ublox7 in overal program to alow compilation

--- xtrex/src/main/java/aprogrammerisneverlate/xtrex/xtrex.java
+++ xtrex/src/main/java/aprogrammerisneverlate/xtrex/xtrex.java
@@ -5,11 +5,13 @@ package aprogrammerisneverlate.xtrex;
 public class xtrex {
     final static String PORT_NAME = "COM6";
     public static void main( String[] argv ) {
-        //Win7Ublox7 GPS = new Win7Ublox7();
-        //GPS.listPorts();
-        //GPS.reader( PORT_NAME );
-        XTrexDisplay disp = XTrexDisplay.getInstance();
-        disp.setScreen(new WhereTo());
+        Win7Ublox7 GPS = new Win7Ublox7();
+        GPS.listPorts();
+        GPS.reader( PORT_NAME );
+
+        //XTrexDisplay disp = XTrexDisplay.getInstance();
+        //disp.setScreen(new WhereTo());
+
     	//MapScreen map = new MapScreen();
     	//disp.setScreen(map);
     	//map.drawMap();
