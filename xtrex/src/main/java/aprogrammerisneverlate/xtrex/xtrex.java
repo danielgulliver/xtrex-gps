8d55272aa3818cfdf68fed134838aeb9a39a63eb
Connor Harris <harrishedge@gmail.com>
2018-02-22 23:43:47 +0000

    Added author & work package

--- xtrex/src/main/java/aprogrammerisneverlate/xtrex/xtrex.java
+++ xtrex/src/main/java/aprogrammerisneverlate/xtrex/xtrex.java
@@ -7,7 +7,14 @@ public class xtrex {
     public static void main( String[] argv ) {
         Win7Ublox7 GPS = new Win7Ublox7();
         GPS.listPorts();
+        
+        System.out.println();
+        System.out.println(GPS.latitude);
+        System.out.println(GPS.longitude);
+        System.out.println("\n\n");
+
         GPS.reader( PORT_NAME );
+        
 
         //XTrexDisplay disp = XTrexDisplay.getInstance();
         //disp.setScreen(new WhereTo());
