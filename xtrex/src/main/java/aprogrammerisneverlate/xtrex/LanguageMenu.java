ee0db553e2b9a943164735364174f31f86962d04
C2P1 <c2p1@users.noreply.github.com>
2018-02-26 21:18:34 +0000

    Created LanguageMenu

---
+++ xtrex/src/main/java/aprogrammerisneverlate/xtrex/LanguageMenu.java
@@ -0,0 +1,20 @@
+package aprogrammerisneverlate.xtrex;
+
+public class LanguageMenu extends Screen {
+
+	@Override
+	public void onMinusButtonPressed() {
+		
+	}
+
+	@Override
+	public void onPlusButtonPressed() {
+		
+	}
+
+	@Override
+	public void onSelectButtonPressed() {
+		
+	}
+
+}
