207846350e86847bac2fe2b60d54941e917ecf86
C2P1 <c2p1@users.noreply.github.com>
2018-02-26 21:27:06 +0000

    getInstance method

--- xtrex/src/main/java/aprogrammerisneverlate/xtrex/LanguageMenu.java
+++ xtrex/src/main/java/aprogrammerisneverlate/xtrex/LanguageMenu.java
@@ -1,6 +1,10 @@
 package aprogrammerisneverlate.xtrex;
 
+import java.awt.GridLayout;
+
 public class LanguageMenu extends Screen {
+
+    private static LanguageMenu languageMenu = null;
 
 	@Override
 	public void onMinusButtonPressed() {
@@ -15,6 +19,17 @@ public class LanguageMenu extends Screen {
 	@Override
 	public void onSelectButtonPressed() {
 		
-	}
+    }
+    
+    private LanguageMenu() {
+        setLayout(new GridLayout(5,1));
+    }
+
+    public static LanguageMenu getInstance() {
+        if (languageMenu == null) {
+            languageMenu = new LanguageMenu();
+        }
+        return languageMenu;
+    }
 
 }
