6b009caba172425b63fefb86a045e02f2f58b398
Laurence <vvanir@gmail.com>
2018-03-08 22:37:45 +0000

    tinkering

--- xtrex/src/main/java/aprogrammerisneverlate/xtrex/xtrex.java
+++ xtrex/src/main/java/aprogrammerisneverlate/xtrex/xtrex.java
@@ -1,28 +1,28 @@
-// package aprogrammerisneverlate.xtrex;
-// /*
-// * Win7 Ublox7 reader.
-// */   
-// public class xtrex {
-//     public static Boolean gpsEnabled = true;
-//     private static Thread gpsThread = null;
+package aprogrammerisneverlate.xtrex;
+/*
+* Win7 Ublox7 reader.
+*/   
+public class xtrex {
+    public static Boolean gpsEnabled = true;
+    private static Thread gpsThread = null;
 
 
-//     public static void main( String[] argv ) {
-//         GPSparser GPS = GPSparser.getInstance();
-//         xtrex.gpsThread = new Thread(GPS, "GPS thread");
+    public static void main( String[] argv ) {
+        GPSparser GPS = GPSparser.getInstance();
+        xtrex.gpsThread = new Thread(GPS, "GPS thread");
         
-//         XTrexDisplay disp = XTrexDisplay.getInstance();
-//         disp.setScreen(MainMenu.getInstance());
+        XTrexDisplay disp = XTrexDisplay.getInstance();
+        disp.setScreen(MainMenu.getInstance());
 
-//         // Start threads.
-//         // TODO: Create and start GPS thread.
-//         //gpsThread.start();
-//         xtrex.gpsThread.run();
-//         new Thread(UpdateThread.getInstance()).start();
-//     }
+        // Start threads.
+        // TODO: Create and start GPS thread.
+        //gpsThread.start();
+        xtrex.gpsThread.run();
+        new Thread(UpdateThread.getInstance()).start();
+    }
 
-// 	public static Thread getGpsThread() {
-//         return gpsThread;
-// 	}
+	public static Thread getGpsThread() {
+        return gpsThread;
+	}
 
-// }
+}
