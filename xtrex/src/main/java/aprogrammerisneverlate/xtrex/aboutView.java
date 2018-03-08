3966572ad3b09d5c7aa1a37ade0a9af4368b19eb
Laurence <vvanir@gmail.com>
2018-03-08 22:35:54 +0000

    tinkering

--- xtrex/src/main/java/aprogrammerisneverlate/xtrex/aboutView.java
+++ xtrex/src/main/java/aprogrammerisneverlate/xtrex/aboutView.java
@@ -1,24 +1,24 @@
-package aprogrammerisneverlate.xtrex;
+// package aprogrammerisneverlate.xtrex;
 
-import java.awt.*;
-import java.io.File;
-import java.io.IOException;
-import java.imageio.imageio;
-import java.swing.*;
+// import java.awt.*;
+// import java.io.File;
+// import java.io.IOException;
+// //import java.imageio.imageio;
+// //import java.swing.*;
 
-public class AboutView extends Screen{
+// public class AboutView extends Screen{
 
-    private static AboutView aboutView = null;
+//     private static AboutView aboutView = null;
 
-    private AboutView() {
+//     private AboutView() {
 
-    }
+//     }
 
-    public static AboutView getInstance() {
-        if (aboutView == null) {
-            aboutView = new AboutView;
-        }
-        return aboutView;
-    }
+//     public static AboutView getInstance() {
+//         if (aboutView == null) {
+//             aboutView = new AboutView();
+//         }
+//         return aboutView;
+//     }
 
-} 
+// } 
