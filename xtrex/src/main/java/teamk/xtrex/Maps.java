afb9fc00f21ef873104cb09f92d328468876a626
C2P1 <c2p1@users.noreply.github.com>
2018-03-17 16:38:08 +0000

    tidying methods

--- xtrex/src/main/java/teamk/xtrex/Maps.java
+++ xtrex/src/main/java/teamk/xtrex/Maps.java
@@ -3,6 +3,7 @@ package teamk.xtrex;
 /**
  * Deals with the map screen and google maps API calls
  * 
+ * @author Adam Griffiths
  * @author Conor Spilsbury
  * 
  * @version Sprint 2
@@ -13,14 +14,11 @@ public class Maps {
 	private static MapModel mapModel;
 	private static MapView mapView;
 	
-	/**
-	 * Constructor
-	 */
+
 	public Maps() {
 		mapModel = MapModel.getInstance();
         mapView = MapView.getInstance();
 		mapController = MapController.getInstance();
-		mapController.updateMap();
 		mapView.setController(mapController);
 		mapController.updateMap();
 	}
@@ -36,7 +34,6 @@ public class Maps {
 		if (mapView == null) {
 			mapView = MapView.getInstance();
 			mapController = MapController.getInstance();
-			mapController.updateMap();
 			mapView.setController(mapController);
 			mapController.updateMap();
 		}
