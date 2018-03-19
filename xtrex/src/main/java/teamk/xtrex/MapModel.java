e4040ffd8ab80d75306df0586b5ae2092d981129
Adam Griffiths <adamfrederickgriffiths@gmail.com>
2018-03-19 22:43:16 +0000

    Fixed prints

--- xtrex/src/main/java/teamk/xtrex/MapModel.java
+++ xtrex/src/main/java/teamk/xtrex/MapModel.java
@@ -114,7 +114,7 @@ public class MapModel {
         if (this.directionLats == null || this.directionIndex >= this.directionLats.length)
             return;
 
-        System.out.println("Next audio play: "+Double.toString(gps.Latitude())+","+Double.toString(gps.Longitude()));
+        System.out.println("Next audio play: "+Double.toString(directionLats[directionIndex])+","+Double.toString(directionLongs[directionIndex]));
         System.out.println("Distance to next audio play: "+new Integer(GPSutil.latLongToDistance(this.directionLats[directionIndex], this.directionLongs[directionIndex], gps.Latitude(), gps.Longitude())).toString());
         //If we are moving away from the next point in the journey we are lost and need to recalculate the journey
         if (!gpsUtil.approaching(directionLats[directionIndex], directionLongs[directionIndex])) {
