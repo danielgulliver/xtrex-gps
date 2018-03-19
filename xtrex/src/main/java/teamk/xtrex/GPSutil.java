60adde832f8260df865f65043afbb1b3b862e578
Connor Harris <harrishedge@gmail.com>
2018-03-19 18:28:44 +0000

    pvt log

---
+++ xtrex/src/main/java/teamk/xtrex/GPSutil.java
@@ -0,0 +1,14 @@
+package teamk.xtrex;
+
+/**
+ * Creates a PVT log to allow historic tracking
+ * 
+ * @author Connor Harris
+ * @version Sprint 3 
+ */
+
+public class GPSutil {
+    PositionVelocityTime[] log = new PositionVelocityTime[50];
+
+    
+}
